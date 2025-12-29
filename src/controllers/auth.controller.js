9ca1de7cd33bb1c98f2938e1a4dfc1111b85c079
Shiva Rajput <shivarajputdpr@gmail.com>
2025-12-29 12:56:41 +0530

    make controller par

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,36 @@
+import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const extingUser = await User.findOne({ where: { id } });
+    if (extingUser) {
+      return res.status(409).json({ message: "User already exit" });
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+    const createUser = await User.create({
+      name,
+      email,
+      password: hashPassword,
+    });
+    return res.status(201).json({
+      message: "User created succesfully",
+      data: {
+        id: User.id,
+        name: User.name,
+        email: User.email,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
+module.exports = { registerUser };

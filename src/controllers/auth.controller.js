c7834bf34232df389e4adfecda5792622ee8bd5f
Shiva Rajput <shivarajputdpr@gmail.com>
2025-12-30 12:08:54 +0530

    Add login API with JWT authentication

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
-import  User  from "../models/user.model.js";
+import User from "../models/user.model.js";
+import Token from "../utils/generateToken.js";
 
 const registerUser = async (req, res) => {
   try {
@@ -33,4 +34,41 @@ const registerUser = async (req, res) => {
   }
 };
 
-export { registerUser };
+
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // 1. Validation
+    if (!email || !password) {
+      return res.status(400).json({ message: "All field required" });
+    }
+
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+    const psdMatch = await bcrypt.compare(password, user.password);
+    if (!psdMatch) {
+      return res.status(401).json({ message: "Password not matching" });
+    }
+
+    const token = Token(user);
+    return res.status(201).json({
+      message: "Login succesful",
+      token,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
+export { registerUser, loginUser };

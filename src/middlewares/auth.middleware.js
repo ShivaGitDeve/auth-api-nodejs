90bb0bb3c13470b20fa0fb01fb3113738e339f2d
Shiva Rajput <shivarajputdpr@gmail.com>
2025-12-30 15:34:45 +0530

    Add JWT authentication middleware for protected routes

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+const proTect = async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  // Token agar missing hua
+  if (!token) {
+    return res.status(401).json({ message: "Token Missing" });
+  }
+
+  // token verify karlo
+
+  try {
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findByPk(decode.id, {
+      attributes: { exclude: ["password"] },
+    });
+    next();
+
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid Token" });
+  }
+};
+
+export default proTect;
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { registerUser, loginUser } from "../controllers/auth.controller.js";
+import proTect from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/profile", proTect, (req, res) => {
+  res.status(200).json({ message: "Profile Data", user: req.user });
+});
 
 export default router;

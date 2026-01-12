7da5ac0cf42f29b425998d23edaf68e2b617d094
Shiva Rajput <shivarajputdpr@gmail.com>
2026-01-12 22:44:25 +0530

    Add AuthContext for frontend authentication state

---
+++ Auth-frontEnd/vite-project/src/auth/Auth-context.jsx
@@ -0,0 +1,72 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import api from "../api/axios";
+
+const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [accessToken, setAccessToken] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // app load par check
+  useEffect(() => {
+    const token = localStorage.getItem("refreshToken");
+    if (token) {
+      refreshAccessToken();
+    } else {
+      setLoading(false);
+    }
+  }, []);
+
+  const login = async (email, password) => {
+    const res = await api.post("/auth/login", { email, password });
+
+    setAccessToken(res.data.accessToken);
+    setUser(res.data.user);
+    localStorage.setItem("refreshToken", res.data.refreshToken);
+  };
+
+  const refreshAccessToken = async () => {
+    try {
+      const refreshToken = localStorage.getItem("refreshToken");
+
+      const res = await api.post(
+        "/auth/refresh-token",
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${refreshToken}`,
+          },
+        }
+      );
+
+      setAccessToken(res.data.accessToken);
+    } catch (err) {
+      logout();
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const logout = () => {
+    setUser(null);
+    setAccessToken(null);
+    localStorage.removeItem("refreshToken");
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        accessToken,
+        loading,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);

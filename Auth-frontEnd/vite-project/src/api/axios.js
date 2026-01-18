0259572c95447a4f6a19cbb732b248f37fce1e4c
Shiva Rajput <shivarajputdpr@gmail.com>
2026-01-18 13:19:09 +0530

    Add axios interceptors for auth token handling

--- Auth-frontEnd/vite-project/src/api/axios.js
+++ Auth-frontEnd/vite-project/src/api/axios.js
@@ -1,7 +1,54 @@
-import axios from "axios"
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api"
-})
+  baseURL: "http://localhost:5000/api",
+});
+
+// Request interceptor (attach access token)
+api.interceptors.request.use(
+  (config) => {
+    const accessToken = sessionStorage.getItem("accessToken");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer $(accessToken)`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+// Response interceptor
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    // access token expired
+    const originalRequest = error.config;
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const refreshToken = localStorage.getItem("refreshToken");
+        const res = await axios.post(
+          "http://localhost:5000/api/auth/refresh-token",
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${refreshToken}`,
+            },
+          },
+        );
+        sessionStorage.setItem("accessToken", res.data.accessToken);
+
+        originalRequest.headers.Authorization = `Bearer ${res.data.accessToken}`;
+
+        return api(originalRequest);
+      } catch (e) {
+        localStorage.removeItem("refreshToken");
+        sessionStorage.removeItem("accessToken");
+        window.location.href = "/login";
+      }
+    }
+    return Promise.reject(error);
+  },
+);
 
 export default api;

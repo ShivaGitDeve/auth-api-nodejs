e86efd490e8ec7fbe18532c264e4207a19f7d363
Shiva Rajput <shivarajputdpr@gmail.com>
2026-01-19 13:48:42 +0530

    make login page with css part but its not complete

---
+++ Auth-frontEnd/vite-project/src/pages/login/Login.jsx
@@ -0,0 +1,48 @@
+
+import { useState } from "react";
+import "./login.css";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-card">
+        <h2>Welcome Back </h2>
+        <p className="subtitle">Login to your account</p>
+
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+
+          <input
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+
+          <button type="submit">Login</button>
+        </form>
+
+        <p className="footer-text">
+          Don’t have an account? <span>Sign Up</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;

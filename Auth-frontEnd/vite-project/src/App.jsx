0cf7658cb49c59e744dd1f8169c92493bae16e0f
Shiva Rajput <shivarajputdpr@gmail.com>
2026-01-23 11:54:35 +0530

    some new changes we add in app.jsx file

--- Auth-frontEnd/vite-project/src/App.jsx
+++ Auth-frontEnd/vite-project/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/login/Login";
 import ProtectedRoute from "./auth/Protected-route";
 import Dashboard from "./pages/dashboard/Dashboard";
+import ForgotPassword from "./pages/Password/Forgot-Password";
+import ResetPassword from "./pages/Password/Reset-Password";
 import "./App.css";
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
 
         <Route
           path="/dashboard"
@@ -18,7 +22,6 @@ function App() {
             </ProtectedRoute>
           }
         />
-        
       </Routes>
     </BrowserRouter>
   );

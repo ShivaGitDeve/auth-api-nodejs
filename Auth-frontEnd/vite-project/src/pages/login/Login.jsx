dbefa75ef5ea643fd6493340e9dc48e69863fee7
Shiva Rajput <shivarajputdpr@gmail.com>
2026-01-30 14:51:21 +0530

    login(!)

--- Auth-frontEnd/vite-project/src/pages/login/Login.jsx
+++ Auth-frontEnd/vite-project/src/pages/login/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
   return (
     <div className="login-container">
       <div className="login-card">
-        <h2>Welcome Back </h2>
+        <h2>Welcome Back! </h2>
         <p className="subtitle">Login to your account</p>
 
         <form onSubmit={handleSubmit}>
